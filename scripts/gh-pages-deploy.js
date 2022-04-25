2cd0cda794bf0dae4ce26007f51ed6cd0470ab6f
Reddevildragg <Thomas.greener@hotmail.co.uk>
2022-04-25 19:33:03 +0100

    More work on auto builds

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,6 +1,7 @@
-const execa = require('execa')
-const emoji = require('node-emoji')
-const chalk = require('chalk')
+import {execa} from "execa";
+import emoji from "node-emoji/lib/emojifile";
+import chalk from "chalk";
+
 
 const firstLog = `${emoji.get('fast_forward')} ${chalk.yellow('Building...')}`
 const secondLog = `${emoji.get('fast_forward')} ${chalk.yellow('Pushing...')}`

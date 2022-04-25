39190941c5e700146068949d1d248daf6f667257
Reddevildragg <Thomas.greener@hotmail.co.uk>
2022-04-25 14:02:26 +0100

    trying to work on auto deployment

---
+++ gh-pages-deploy.mjs
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+
+import {execa} from "execa";
+import {existsSync} from "fs";
+
+(async () => {
+    try {
+        const {stdout} = await execa("git", ["checkout", "--orphan", "gh-pages"]);
+        console.log(stdout);
+        // eslint-disable-next-line no-console
+        console.log("Building started...");
+        await execa("npm", ["run", "build"]);
+        // await execa("yarn", ["build"]);
+        // Understand if it's dist or build folder
+        const folderName = existsSync("dist") ? "dist" : "build";
+        await execa("git", ["--work-tree", folderName, "add", "--all"]);
+        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        console.log("Pushing to gh-pages...");
+        await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+
+        await execa("git", ["checkout", "-f", "master"]);
+        await execa("git", ["branch", "-D", "gh-pages"]);
+        console.log("Successfully deployed, check your settings");
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e.message);
+        process.exit(1);
+    }
+})();

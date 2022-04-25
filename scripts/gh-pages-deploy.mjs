9c5fd6bd875d04fb7ccf1d2eaa5316deaea7daad
Reddevildragg <Thomas.greener@hotmail.co.uk>
2022-04-25 19:47:27 +0100

    undo everything to try again

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -1,22 +1,28 @@
+/* eslint-disable no-console */
+
 import {execa} from "execa";
+import {existsSync} from "fs";
 
-
-;(async () => {
+(async () => {
   try {
-    const { stdout: currentBranch } = await execa.command('git branch --show-current')
-    await execa.command('git checkout --orphan gh-pages')
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("npm", ["run", "build"]);
+    // await execa("yarn", ["build"]);
+    // Understand if it's dist or build folder
+    const folderName = existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
 
-    await execa.command('npm run build', { stdio: 'inherit' })
-    await execa.command('git --work-tree dist add --all')
-    await execa.command('git --work-tree dist commit -m "gh-pages"')
-
-    await execa.command('git push origin HEAD:gh-pages --force', { stdio: 'inherit' })
-    //await execa.command('rm -r dist')
-    await execa.command(`git checkout -f ${currentBranch}`)
-    await execa.command('git branch -D gh-pages')
-
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
   } catch (e) {
-    console.log(e.message)
-    process.exit(1)
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
   }
-})()
+})();
